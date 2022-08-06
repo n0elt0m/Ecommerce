050433458909b2d543a565d008bf2ec55d9036a1
NOEL MUNDACKAL <noeltom15@gmail.com>
2022-08-06 16:53:21 +0530

    8

--- database/cart.go
+++ database/cart.go
@@ -1,8 +1,14 @@
 package database
 
 import (
+	"context"
 	"errors"
-	"github.com/gin-gonic/gin"
+	"github.com/noel/ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"time"
 )
 
 var (
@@ -15,18 +21,71 @@ var (
 	ErrCantBuyCartItem    = errors.New("cant purchase")
 )
 
-func AddProductToCart() gin.HandlerFunc {
+func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productId primitive.ObjectID, userId string) error {
+	searchFromDb, err := prodCollection.Find(ctx, bson.M{"_id": productId})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+	var productCart []models.ProductUser
+	err = searchFromDb.All(ctx, &productCart)
+	if err != nil {
+		log.Println(err)
+		return ErrCantDecodeProduct
+	}
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productCart}}}}}}
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+	return nil
+}
+
+func RemoveCartItem(ctx context.Context, prodCollection, userCollection *mongo.Collection, productId primitive.ObjectID, userId string) error {
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.M{"$pull": bson.M{"usercart": bson.M{"_id": productId}}}
+	_, err = userCollection.UpdateMany(ctx, filter, update)
+	if err != nil {
+		return ErrCantRemoveItemCart
+	}
+	return nil
 
 }
 
-func RemoveCartItem() gin.HandlerFunc {
+func BuyItemFromCart(ctx context.Context, userCollection *mongo.Collection, userID string) error {
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+	var getcartitems models.User
+	var ordercart models.Order
 
+	ordercart.Order_ID = primitive.NewObjectID()
+	ordercart.Orderered_At = time.Now()
+	ordercart.Order_Cart = make([]models.ProductUser, 0)
+	ordercart.Payment_Method.COD = true
+
+	unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$usercart"}}}}
+	grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: "$_id"}, {Key: "total", Value: bson.D{primitive.E{Key: "$sum", Value: "$usercart.price"}}}}}}
+	currentResults, err := userCollection.Aggregate(ctx, mongo.Pipeline{unwind, grouping})
+	ctx.Done()
+	if err != nil {
+		panic(err)
+	}
 }
 
-func BuyItemFromCart() gin.HandlerFunc {
-
-}
-
-func InstantBuy() gin.HandlerFunc {
+func InstantBuy() {
 
 }
