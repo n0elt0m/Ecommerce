30299dce01919d7dbbd6ba3f0b6598a1f695b600
NOEL MUNDACKAL <noeltom15@gmail.com>
2022-08-06 13:36:42 +0530

    7

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,26 @@
 package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/noel/ecommerce/tokens"
+	"net/http"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No authorization Header Provided"})
+			return
+		}
+		claims, err := tokens.ValidateToken(ClientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
